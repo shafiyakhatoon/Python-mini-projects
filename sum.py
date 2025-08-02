ab910559c1420ef9a9c0f58ea64ed92d62dad964
Shafiya Khatoon <shafiyakhatoon2006@gmail.com>
2025-08-02 10:09:35 +0530

    Delete sum.py

--- sum.py
+++
@@ -1,8 +0,0 @@
-a = input("a = ")
-b = input("b = ")
-sum = a + b
-print(sum)
-#if a = 1 b = 2 sum = 12 //// as input converts this into str
-sum = int(a) + int(b) #by using this we convert into int int()
-print(sum)
-#if a = 1 b = 2 sum = 3
